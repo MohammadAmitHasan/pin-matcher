55638e6bcb8254124dd0cfe780cbf78a1fc1c431
MohammadAmitHasan <bhamithasan@gmail.com>
2022-02-13 17:22:29 +0600

    authentication done

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ function generatePin() {
 document.getElementById('calc-buttons').addEventListener('click', function (event) {
     const button = event.target.innerText;
     const screen = document.getElementById('screen-button');
-
     if (isNaN(button)) {
         if (button == 'C') {
             screen.value = '';
+            document.getElementById('notify-success').style.display = 'none';
+            document.getElementById('notify-fail').style.display = 'none';
         }
         if (button == '<') {
             screen.value = screen.value.slice(0, -1);
@@ -27,5 +28,17 @@ document.getElementById('calc-buttons').addEventListener('click', function (even
     else {
         screen.value += button;
     }
+});
 
+document.getElementById('code-submit').addEventListener('click', function () {
+    const screenButtonValue = document.getElementById('screen-button').value;
+    const screenPinValue = document.getElementById('screen-pin').value;
+    if (screenButtonValue == screenPinValue) {
+        document.getElementById('notify-success').style.display = 'block';
+        document.getElementById('notify-fail').style.display = 'none';
+    }
+    else {
+        document.getElementById('notify-success').style.display = 'none';
+        document.getElementById('notify-fail').style.display = 'block';
+    }
 })
